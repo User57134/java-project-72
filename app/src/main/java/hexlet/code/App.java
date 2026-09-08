9aeab1f92845efd1e39a650004bcf4adfa858cc4
Player One <pl17dev@gmail.com>
2026-09-08 12:20:44 +0300

    Шаблонизатор: настроен режим разработки

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,6 +4,7 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import gg.jte.ContentType;
 import gg.jte.TemplateEngine;
+import gg.jte.resolve.DirectoryCodeResolver;
 import gg.jte.resolve.ResourceCodeResolver;
 import hexlet.code.repository.BaseRepository;
 import io.javalin.Javalin;
@@ -12,10 +13,23 @@ import io.javalin.rendering.template.JavalinJte;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.SQLException;
 import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class App {
+    private static final Path TEMPLATES_PATH = Path.of("src", "main", "resources", "templates");
+    private static final Path STATIC_PATH = Path.of("src", "main", "resources", "static");
+    private static final Path JTE_CLASSES_PATH = Path.of("jte-classes");
+
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    private static String getMode() {
+        return System.getenv().getOrDefault("APP_ENV", "production");
+    }
 
     private static int getPort() {
         // Получаем url базы данных из переменной окружения DATABASE_URL
@@ -34,7 +48,16 @@ public class App {
                 .getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1");
     }
 
+    private static boolean isDevelopment() {
+        return getMode().equals("development") && Files.isDirectory(TEMPLATES_PATH);
+    }
+
     private static TemplateEngine createTemplateEngine() {
+        if (isDevelopment()) {
+            var codeResolver = new DirectoryCodeResolver(TEMPLATES_PATH);
+            return TemplateEngine.create(codeResolver, JTE_CLASSES_PATH, ContentType.Html);
+        }
+
         var classLoader = App.class.getClassLoader();
         var codeResolver = new ResourceCodeResolver("templates", classLoader);
         return TemplateEngine.create(codeResolver, ContentType.Html);
@@ -56,6 +79,10 @@ public class App {
     }
 
     public static Javalin getApp() {
+        logger.info(
+                "Applications is started in "
+                        + (isDevelopment() ? " a development mode" : "production mode"));
+
         var hikariConfig = new HikariConfig();
 
         hikariConfig.setJdbcUrl(getDatabaseUrl());
@@ -77,7 +104,11 @@ public class App {
                 Javalin.create(
                         config -> {
                             config.bundledPlugins.enableDevLogging();
-                            config.staticFiles.add("/static", Location.CLASSPATH);
+                            if (isDevelopment()) {
+                                config.staticFiles.add(STATIC_PATH.toString(), Location.EXTERNAL);
+                            } else {
+                                config.staticFiles.add("/static", Location.CLASSPATH);
+                            }
                             config.fileRenderer(new JavalinJte(createTemplateEngine()));
                             config.routes.get("/", ctx -> ctx.render("index.jte"));
                         });
