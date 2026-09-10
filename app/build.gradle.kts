7cba6b347bd1dcb57446581f965d8d225870a2c5
Player One <pl17dev@gmail.com>
2026-09-10 13:50:39 +0300

    Анализ страницы: подключена база данных PostgreSQL

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,9 @@ dependencies {
     // Подключаем библиотеку commons-lang3-lib для обработки чисел
     implementation(libs.commons.lang3)
 
+    // Подключение драйвера Postgresql
+    implementation(libs.postgresql)
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
