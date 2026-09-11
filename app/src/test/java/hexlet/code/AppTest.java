5715adfe88f2d94dc4a38210fe88d4268acd5132
Player One <pl17dev@gmail.com>
2026-09-11 11:24:59 +0300

    Тестирование: обновлена версия библиотеки javalin, также в каталоге версии сделано, чтобы созависимыее библиотеки ссылались на общую версию javalin

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -3,9 +3,12 @@ package hexlet.code;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import hexlet.code.repository.UrlRepository;
+import hexlet.code.util.NamedRoutes;
 import io.javalin.Javalin;
 import io.javalin.testtools.JavalinTest;
+import io.javalin.testtools.TestConfig;
 import java.io.IOException;
+import java.net.http.HttpClient;
 import java.sql.SQLException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -29,6 +32,22 @@ public class AppTest {
                     var response = client.get("/");
                     assertThat(response.code()).isEqualTo(200);
                     assertThat(response.body().string()).contains("Анализатор страниц");
+                });
+    }
+
+    @Test
+    public void testUrls() {
+        var httpClient =
+                HttpClient.newBuilder().followRedirects(HttpClient.Redirect.NORMAL).build();
+
+        var config = new TestConfig(true, true, httpClient);
+
+        JavalinTest.test(
+                app,
+                config,
+                (server, client) -> {
+                    var response = client.post(NamedRoutes.urlsPath(), "url=https://example.com");
+                    assertThat(response.code()).isEqualTo(200);
                 });
     }
 }
